d78ffafb8d2f0d40b443ef7256535356fcec169d
shabnamsha13 <148140705+shabnamsha13@users.noreply.github.com>
2026-02-07 17:23:27 +0530

    Add files via upload

---
+++ Day-4/loops/while_loop.py
@@ -0,0 +1,12 @@
+#Print numbers from 1 to 5 using while loop
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+#Sum of numbers from 1 to 5 using while loop
+sum = 0
+i = 1
+while i <= 5:
+    sum += i
+    i += 1
+print("Sum:", sum)
