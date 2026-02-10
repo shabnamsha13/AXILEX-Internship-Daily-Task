fe93b809c40f4c95f905ac2d61e879d5cfee74e8
shabnamsha13 <148140705+shabnamsha13@users.noreply.github.com>
2026-02-10 11:39:20 +0530

    Add files via upload

---
+++ Week-2/Day-1/large_small.py
@@ -0,0 +1,8 @@
+# Sample list
+numbers = [10, 45, 2, 78, 33, 5]
+
+# Using built-in functions
+largest = max(numbers)
+smallest = min(numbers)
+print("Largest number:", largest)
+print("Smallest number:", smallest)
