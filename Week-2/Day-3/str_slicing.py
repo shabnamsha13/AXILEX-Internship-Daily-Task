ce72c315dfc6d93a915d77de0f93a6f09bc4daad
shabnamsha13 <148140705+shabnamsha13@users.noreply.github.com>
2026-02-12 10:23:34 +0530

    Add files via upload

---
+++ Week-2/Day-3/str_slicing.py
@@ -0,0 +1,7 @@
+#string Slicing Operations
+text = input("Enter a string: ")
+print("Original String:", text)
+print("First 3 characters:", text[0:3])
+print("Last 3 characters:", text[-3:])
+print("Every second character:", text[::2])
+print("Reversed String:", text[::-1])
